bdb7aec1e87d33f9b043a54e8ca0915538ba9872
Andrey Aksenov <tombery1@ya.ru>
2015-11-05 09:53:18 +0300

    add Company and relation to User (h_m through)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+
+  has_many :projects
+  has_many :company_users
+  has_many :companies, through: :company_users
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
